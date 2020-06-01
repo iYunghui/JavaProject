5100a9b9c5d84debed172999d718a91268b694fc
Tzu1999 <56683292+Tzu1999@users.noreply.github.com>
2020-06-01 15:54:40 +0800

    Update move

--- Role.java
+++ Role.java
@@ -1,33 +1,32 @@
 import java.util.*;
 import java.math.*;
-
 public class Role {
 	private int blood;
 	private int pos_x, pos_y;
 	private long skill2time = 0;
 	private long skill3time = 0;
+	private int tower_x = 200;
+	private int tower_y = 250;
 	
 	public int skill_one(int towerblood) {
-		/*if(pox_x > tower_x(左邊) && pos_x < tower_x(右邊))
-		 * 	return towerblood - 5;
-		 *else if(pox_y > tower_y(上面) && pos_y < tower_y(下面))
-		 * 	return towerblood - 5;
-		 * */
-		return towerblood - 400;
+		if(pos_x > tower_x && pos_x < tower_x + 250)
+		 	return towerblood - 5;
+		else if(pos_y > tower_y && pos_y < tower_y + 150)
+		 	return towerblood - 5;
+		return towerblood;
 	}
-	
 	public int skill_two(int towerblood) {
-		long current = System.currentTimeMillis( );
-		/*if(pox_x > tower_x(左邊) && pos_x < tower_x(右邊))
-		 * else if(pox_y > tower_y(上面) && pos_y < tower_y(下面))*/
-		if(skill2time == 0 || current - skill2time >= 15000) {
+		if(pos_x < tower_x || pos_x > tower_x + 250)
+		 	return towerblood;
+		else if(pos_y < tower_y || pos_y > tower_y + 150)
+		 	return towerblood;
+		else if(skill2time == 0 || current - skill2time >= 15000) {
 			skill2time = current;
-			return towerblood - 20;
+			return towerblood - 10;
 		}
 		else 
 			return towerblood;
 	}
-	
 	public int skill_three(int towerblood) {
 		long current = System.currentTimeMillis( );
 		if(skill3time == 0 || current - skill3time >= 15000) {
@@ -41,38 +40,34 @@ public class Role {
 		else
 			return towerblood;
 	}
-	
 	public void move(int x, int y) {
-					
+		if(pos_x + x > 0 && pos_x + x < 1080)
+			pos_x = pos_x + x;
+		if(pos_y + y > 0 && pos_y + x < 720)
+			pos_y = pos_y + y;
 	}
-	
-	public Role(int b, int x, int y) {
-		blood = b;
-		pos_x = x;
-		pos_y = y;
+	public Role() {
+		blood = 1500;
+		pos_x = 0;
+		pos_y = 300;
 	}
-	
 	public int getblood() {
 		return blood;
 	}
-	
 	public int getposx() {
 		return pos_x;
 	}
-	
 	public int getposy() {
 		return pos_y;
 	}
-	
 	public void setblood(int change) {
 		blood = blood + change;
 	}
-	
 	public void setposx(int change) {
 		pos_x = pos_x + change;
 	}
-	
 	public void setposy(int change) {
 		pos_y = pos_y + change;
 	}
 }
+
