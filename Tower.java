abb2a3347d1d399477cbfd978f6381bbe15b43a7
Lucia28 <56835516+Lucia28@users.noreply.github.com>
2020-06-01 15:50:08 +0800

    Update Tower.java

--- Tower.java
+++ Tower.java
@@ -9,17 +9,8 @@ public class Tower {
 	private int cha_x;
 	private int cha_y;
 	
-	public void setblood(int attack) {
-		if (attack == 1)
-			blood = blood - 5;
-		else if (attack == 2) {
-			blood = blood - 10;
-			blood = blood - 10;
-		}
-		else if (attack == 3)
-			blood = blood - 50;
-		else if (attack == 4)
-			blood = blood - 100;
+	public void setblood() {
+		blood = blood - 10;
 	}
 	
 	public int getblood() {
@@ -37,22 +28,6 @@ public class Tower {
 	
 	public int getposition_y() {
 		return position_y;
-	}
-	
-	/*
-	Timer timer = new Timer();
-	TimerTask task = new TimerTask() {
-		public boolean attack() {
-			return true;
-		}
-	};
-		
-	timer.schedule(task, 1000, 1000);
-	*/
-	
-	public boolean attack() {
-		//delay 1s
-		return true;
 	}
 }
 
